3284de809117150564b5a062a1abfa559773d477
pramodkulkarniRK <pramodcserymec@gmail.com>
2026-03-29 11:39:45 +0530

    updating

---
+++ loops/ec2-create.tf
@@ -0,0 +1,33 @@
+# Creating EC2 Instance
+resource "aws_instance" "terraform_instance" {
+    ami = var.ami_id  
+    instance_type = var.instance_type
+    vpc_security_group_ids = [aws_security_group.allow-all.id]    
+    count=4   
+
+    tags = {
+      Name = var.instances[count.index]
+      Terraform = "True"
+    }
+}
+
+# Creating Security Group
+resource "aws_security_group" "allow-all" {
+    name = var.sg_name
+
+    egress  {
+      from_port = var.egress_from_port # From port 0 to port 0 means all ports
+      to_port = var.egress_to_port
+      protocol = var.protocol  # -1 means all protocols
+      cidr_blocks = var.cidr  #internet wide access
+    }
+
+    ingress {
+      from_port = var.ingress_from_port # From port 0 to port 0 means all ports
+      to_port = var.ingress_to_port
+      protocol = var.protocol  # -1 means all protocols
+      cidr_blocks = var.cidr  #internet wide access
+    }
+
+    tags = var.sg_tags
+}
