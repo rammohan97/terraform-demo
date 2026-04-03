cb0bfc3e4aeca10e6164847100deeaf57fe3183f
pramodkulkarniRK <pramodcserymec@gmail.com>
2026-04-03 14:52:12 +0530

    updating

--- provisioners/ec2-create.tf
+++ provisioners/ec2-create.tf
@@ -15,6 +15,28 @@ resource "aws_instance" "terraform_instance" {
         command = "echo 'EC2 Instance ${self.id} has been destroyed'"
         when = destroy
     }
+
+    connection {
+      type = "ssh"
+      user = "ec2-user"
+      password = "DevOps321"
+      host = self.public_ip 
+    }
+
+    provisioner "remote-exec" {
+      inline = [
+        "sudo dnf install nginx -y",
+        "sudo systemctl start nginx",
+      ]
+    }
+
+    provisioner "remote-exec" {
+      inline = [
+        "sudo systemctl stop nginx",
+        "echo successfully stopped nginx",
+      ]
+      when = destroy
+    }
 }
 
 # Creating Security Group
